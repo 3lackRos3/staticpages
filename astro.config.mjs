0bbef5fb6b921a3c8e5d2b02fd4864501e2c9691
Prem Anand <prem89k@gmail.com>
2025-08-27 23:07:13 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'static'
+  site: 'https://staticpages.site',
+  output: 'static',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      entryLimit: 10000,
+    })
+  ]
 });
